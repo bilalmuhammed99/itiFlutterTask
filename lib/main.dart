7248121dee762f256f4301b694fd3c1eab5bdb72
Bilal Muhammed <belal9971@yahoo.com>
2025-03-11 21:08:27 +0200

    Messanger

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_iti_task/new_screen.dart';
 import 'package:flutter_iti_task/vlidation_form_screen.dart';
 
+import 'messanger_screen.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -12,7 +14,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginScreen(),
+      home: MessengerScreen(),
     );
   }
 }
